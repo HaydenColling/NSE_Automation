5953c7f6cd2d7673b17dcfaeeaf0ae2e819a8d94
HaydenColling <91355627+HaydenColling@users.noreply.github.com>
2023-06-03 17:09:35 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 def install(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
-# Add all the packages to the list that you want to install
+# All the packages that are required
 packages = ['requests', 'beautifulsoup4', 'nltk', 'scikit-learn', 'joblib', 'json']
 
 for package in packages:
