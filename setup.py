f3b273eef26e2860d6daca2b9c486b4856a574ac
HaydenColling <91355627+HaydenColling@users.noreply.github.com>
2023-06-03 21:01:54 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ import sys
 
 def install(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+    if package is "nltk":
+        import nltk
+        nltk.download('stopwords')
 
 packages = ['requests', 'beautifulsoup4', 'nltk', 'scikit-learn', 'joblib', 'json']
 
