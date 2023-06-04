84069b497371845437592a96025bfc31dd860bd7
HaydenColling <91355627+HaydenColling@users.noreply.github.com>
2023-06-03 20:57:09 -0500

    Add files via upload

--- setup.py
+++ setup.py
@@ -4,8 +4,10 @@ import sys
 def install(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
-# All the packages that are required
 packages = ['requests', 'beautifulsoup4', 'nltk', 'scikit-learn', 'joblib', 'json']
 
 for package in packages:
     install(package)
+
+import nltk
+nltk.download('stopwords')
